6192a183cb8199d4cd69df4f89ae25eea3505987
alex <alex.korolev96@gmail.com>
2023-02-02 20:39:37 +0300

    Добавил задачу №1, ДЗ №4

---
+++ Fourth lesson/Task_1/Program4.java
@@ -0,0 +1,26 @@
+//1. Сохранить в файл строку и загрузить из файла строку с выводом в консоль используя классы FileWriter и FileReader
+
+import java.io.*;
+import java.nio.charset.*;
+
+public class Program4 {
+
+    public static void main(String[] args) {
+        try {
+			FileWriter writingToFile = new FileWriter("Fourth lesson/Task_1/Task_1.txt", Charset.forName("UTF-8")); // запись в файл
+			writingToFile.append("Работа с файлом: загрузка строки в файл!");
+			writingToFile.flush();
+
+			String line = "";
+			FileReader readingFromFile = new FileReader("Fourth lesson/Task_1/Task_1.txt", Charset.forName("UTF-8")); // чтение из файла
+			while (readingFromFile.ready()) {
+				line += (char)readingFromFile.read();
+			}
+
+			System.out.println("\n\033[34m" + line + "\033[0m");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+}
